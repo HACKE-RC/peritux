9ee1be0c431e6b93eb024e716602c2b0655a826d
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2023-04-08 14:51:38 +0530

    Update main.cpp
    Added section parsing and Imports parsing.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,22 @@
 #include <vector>
 
 std::string returnArch(const int machineArch);
+DWORD resolveRVAtoFileOffset(DWORD RVA, WORD& ImgTotalSections, PIMAGE_SECTION_HEADER pImgSectionHeader);
 void parseFileHeader(IMAGE_FILE_HEADER& ImgFileHeader);
 void parseOptionalHeader(IMAGE_OPTIONAL_HEADER& ImgOptionalHeader);
-void parseSectionHeaders(IMAGE_SECTION_HEADER* pImgSectionHeader, WORD& ImgNoOfSections);
+void parseSectionHeaders(PIMAGE_SECTION_HEADER pImgSectionHeader, WORD& ImgNoOfSections);
+void parseImports(PIMAGE_DATA_DIRECTORY pImageDataDirectory, WORD& ImgTotalSections, PIMAGE_SECTION_HEADER pImgSectionHeader, unsigned char* pBuffer);
+void parseExports(PIMAGE_DATA_DIRECTORY pImageDataDirectory, WORD& ImgTotalSections, PIMAGE_SECTION_HEADER pImgSectionHeader, unsigned char* pBuffer);
+
+bool isDLL = false;
 
 int main(int argc, char* argv[]){
-   if (argc != 2){
-      std::cout << "Please provide the filename!" << std::endl;
-      return -1;
-   }
+//    if (argc != 2){
+//       std::cout << "Please provide the filename!" << std::endl;
+//       return -1;
+//    }
 
-    HANDLE hFile = CreateFile(argv[1], GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hFile = CreateFile("C:\\Windows\\System32\\iri.dll", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     if (hFile == INVALID_HANDLE_VALUE){
         std::cout << "[ERR] The provided file does not exist!" << std::endl;
@@ -52,17 +57,22 @@ int main(int argc, char* argv[]){
 
     IMAGE_FILE_HEADER ImgFileHeader = (IMAGE_FILE_HEADER)(pNtHeaders->FileHeader);
     IMAGE_OPTIONAL_HEADER ImgOptionalHeader = (IMAGE_OPTIONAL_HEADER)(pNtHeaders->OptionalHeader);
+    PIMAGE_DATA_DIRECTORY pImgDataDirectory = (PIMAGE_DATA_DIRECTORY)ImgOptionalHeader.DataDirectory;
+    pImgDataDirectory++;
 
     WORD ImgTotalSections = ImgFileHeader.NumberOfSections;
     PIMAGE_SECTION_HEADER pImgSectionHeader = IMAGE_FIRST_SECTION(pNtHeaders);
 
     parseSectionHeaders(pImgSectionHeader, ImgTotalSections);
 
-    std::cout << "Signature: 0x" << std::hex << pNtHeaders->Signature << std::endl;
+    std::cout << "Signature: 0x" << std::hex << pNtHeaders->Signature << std::dec << std::endl;
     parseFileHeader(ImgFileHeader);
     parseOptionalHeader(ImgOptionalHeader);
-
-
+    parseImports(pImgDataDirectory, ImgTotalSections, pImgSectionHeader, pBuffer);
+    // Parsing exports only if the PE is a DLL.
+    if (isDLL){
+        parseExports(pImgDataDirectory, ImgTotalSections, pImgSectionHeader, pBuffer);
+    }
 
     delete[] pBuffer;
 }
@@ -111,6 +121,7 @@ void parseFileHeader(IMAGE_FILE_HEADER& ImgFileHeader){
     std::cout << "Relocs stripped: " << std::boolalpha << ((ImgFileHeader.Characteristics & IMAGE_FILE_RELOCS_STRIPPED) != 0) << std::endl;
     std::cout << "Executable: " << ((ImgFileHeader.Characteristics & IMAGE_FILE_EXECUTABLE_IMAGE) != 0) << std::endl;
     std::cout << "DLL: " << ((ImgFileHeader.Characteristics & IMAGE_FILE_DLL) != 0) << std::endl;
+    isDLL = ((ImgFileHeader.Characteristics & IMAGE_FILE_DLL) != 0);
     std::cout << "Debug stripped: " << ((ImgFileHeader.Characteristics & IMAGE_FILE_DEBUG_STRIPPED) != 0) << std::endl;
 }
 
@@ -122,8 +133,7 @@ void parseOptionalHeader(IMAGE_OPTIONAL_HEADER& ImgOptionalHeader){
     std::cout << "Subsystem: " << returnImageSubsystem(ImgOptionalHeader.Subsystem) << std::endl;
 }
 
-void parseSectionHeaders(IMAGE_SECTION_HEADER* pImgSectionHeader, WORD& ImgNoOfSections){
-
+void parseSectionHeaders(PIMAGE_SECTION_HEADER pImgSectionHeader, WORD& ImgNoOfSections){
     for (int i = 0; i < ImgNoOfSections; i++) {
         std::cout << i+1 << ". " << pImgSectionHeader->Name << std::endl;
         std::cout << "\tVirtual Size: " << pImgSectionHeader->Misc.VirtualSize << std::endl;
@@ -134,6 +144,68 @@ void parseSectionHeaders(IMAGE_SECTION_HEADER* pImgSectionHeader, WORD& ImgNoOfS
         std::cout << "\t\tMEM_SHARED: " << std::boolalpha << ((pImgSectionHeader->Characteristics & IMAGE_SCN_MEM_SHARED) != 0)<< std::endl;
         std::cout << "\t\tCNT_CODE: " << std::boolalpha << ((pImgSectionHeader->Characteristics & IMAGE_SCN_CNT_CODE) != 0)<< std::endl;
         pImgSectionHeader++;
+    }
+
+}
+
+DWORD resolveRVAtoFileOffset(DWORD RVA, WORD& ImgTotalSections, PIMAGE_SECTION_HEADER pImgSectionHeader){
+    int index;
+
+    for (int i = 0; i < ImgTotalSections; i++){
+        if (RVA >= pImgSectionHeader->VirtualAddress && RVA < (pImgSectionHeader->VirtualAddress +pImgSectionHeader->Misc.VirtualSize)){
+            index = i;
+            break;
+        }
+        pImgSectionHeader++;
+    }
+
+    return (RVA - pImgSectionHeader->VirtualAddress) + pImgSectionHeader->PointerToRawData;
+}
+
+void parseImports(PIMAGE_DATA_DIRECTORY pImageDataDirectory, WORD& ImgTotalSections, PIMAGE_SECTION_HEADER pImgSectionHeader, unsigned char* pBuffer){
+
+//    f -> file offset
+    DWORD fDataDirectory = resolveRVAtoFileOffset(pImageDataDirectory->VirtualAddress, ImgTotalSections, pImgSectionHeader);
+    PIMAGE_IMPORT_DESCRIPTOR pImgImportDescriptor;
+    PIMAGE_IMPORT_DESCRIPTOR pTemp;
+    int totalDirectory = 0;
+
+    pImgImportDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(pBuffer + fDataDirectory);
+    pTemp = (PIMAGE_IMPORT_DESCRIPTOR)(pBuffer + fDataDirectory);
+
+    while (true){
+        if (pImgImportDescriptor->Name == 0x00000000 && pImgImportDescriptor->FirstThunk == 0x00000000){
+            totalDirectory -= 1 ;
+            break;
+        }
+        pImgImportDescriptor++;
+        totalDirectory++;
+    }
+    std::cout << "Total Imports: " << totalDirectory << std::endl;
+
+    pImgImportDescriptor = pTemp;
+
+    for (int i = 0; i <= totalDirectory; i++){
+        char* dllName = (char*)(pBuffer + resolveRVAtoFileOffset(pImgImportDescriptor->Name, ImgTotalSections, pImgSectionHeader));
+
+        // getting the Import Lookup Table address from Import Table
+        // original first thunk = ILT
+        PIMAGE_THUNK_DATA pILT = (PIMAGE_THUNK_DATA)(pBuffer + resolveRVAtoFileOffset(pImgImportDescriptor->OriginalFirstThunk, ImgTotalSections, pImgSectionHeader));
+
+        std::cout << "\t" << i+1 << ". " << dllName << std::endl;
+        while (pILT->u1.AddressOfData != 0x00000000){
+            if (pILT->u1.Ordinal & IMAGE_ORDINAL_FLAG){
+                WORD ordinal = (WORD)(pILT->u1.Ordinal & ~IMAGE_ORDINAL_FLAG);
+                std::cout << "\t[O]: " << ordinal << std::endl;
+            }
+            else{
+                auto pImgImportByName = (PIMAGE_IMPORT_BY_NAME)(pBuffer+ resolveRVAtoFileOffset(pILT->u1.AddressOfData, ImgTotalSections, pImgSectionHeader));
+                std::cout << "\t[N]: " << pImgImportByName->Name << std::endl;
+            }
+            pILT++;
+        }
+
+        pImgImportDescriptor++;
     }
 
 }
